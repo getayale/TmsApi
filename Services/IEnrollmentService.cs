68d4db24fa947039e94fac752bd8196bb22e4571
getayale <teshalegetayalew.com>
2026-08-03 12:36:34 +0300

    feat: define enrollment service contract

---
+++ Services/IEnrollmentService.cs
@@ -0,0 +1,11 @@
+namespace TmsApi.Services;
+public interface IEnrollmentService
+{
+    Task<EnrollmentRecord> EnrollAsync(string studentId, string courseCode);
+
+    Task<EnrollmentRecord?> GetByIdAsync(string id);
+
+    Task<IReadOnlyList<EnrollmentRecord>> GetAllAsync();
+
+    Task<bool> DeleteAsync(string id);
+}
